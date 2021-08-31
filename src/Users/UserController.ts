42e63cf6329042d22252721f05e7bb25b50eca3e
jwoo9928 <astro9928@o.cnu.ac.kr>
2021-08-31 18:10:52 +0900

    mysql

--- src/Users/UserController.ts
+++ src/Users/UserController.ts
@@ -1,10 +1,17 @@
 import { NextFunction, Response, Request } from "express";
+const sqlServer = require('../../config/db');	//사용자 정의한 함수 사용
+const connect = sqlServer.dbconnection.init();
 
 class UserController {
 	public getUserName = (req: Request, res: Response, next: NextFunction) => {
 		console.log("activate get user Name");
-		res.json({
-			"result" : req.params.id
+		const sql = `select name from users where id='${req.params.id}'`;
+		connect.query(sql, (err, result, fields) => {
+			if(err) throw err;
+			res.json(
+				result
+			);
+	
 		});
 	}
 }
--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import UserRouter from './Users/UserRouter';
 
 const app: express.Application = express();
 const port = process.env.PORT || 3000;
+// const sqlServer = 
+// const connect = sqlServer.dbconnection.init();
 
 app.use("/user", UserRouter);
 
